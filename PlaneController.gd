a57aab3b2446027792223ad6fb7b3df8d3964e19
FireEntity1 <134467282+FireEntity1@users.noreply.github.com>
2024-06-27 23:32:16 -0600

    Vertical movement

--- PlaneController.gd
+++ PlaneController.gd
@@ -8,12 +8,22 @@ var gravity = ProjectSettings.get_setting("physics/3d/default_gravity")
 
 
 func _physics_process(delta):
-	# Add the gravity.
+	var input_dir = Input.get_vector("RudderLeft", "RudderLeft", "ElevatorUp", "ElevatorDown")
+	var direction = (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
 	if not is_on_floor():
 		velocity.y -= gravity * delta
 	throttleControl()
 	moveForwards()
+	elevator()
+	rudder()
 	fly()
+
+	#if direction:
+		#velocity.z = direction.z * speed
+		#velocity.x = direction.x * speed
+	#else:
+		#velocity.x = move_toward(velocity.x, 0, speed)
+		#velocity.z = move_toward(velocity.z, 0, speed)
 
 	move_and_slide()
 
@@ -24,6 +34,19 @@ func throttleControl():
 		throttle -= 1
 	$PlayerCam/Control/ProgressBar.value = throttle
 
+func elevator():
+	if speed > 10:
+		if Input.is_action_pressed("ElevatorUp"):
+			self.rotation.x += 0.02
+		if Input.is_action_pressed("ElevatorDown"):
+			self.rotation.x -= 0.02
+
+func rudder():
+	if Input.is_action_pressed("RudderRight"):
+		rotation.y += 0.02
+	if Input.is_action_pressed("RudderLeft"):
+		rotation.y -= 0.02
+
 func moveForwards():
 	if speed < throttle:
 		speed += 0.05
@@ -32,5 +55,5 @@ func moveForwards():
 	self.velocity = Vector3(velocity.x, velocity.y, speed)
 
 func fly():
-	if speed > 13:
-		velocity.y = speed/5
+	if speed > 15:
+		velocity.y = speed/5 * rotation.x * 5
